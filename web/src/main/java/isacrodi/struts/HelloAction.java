65b48c8334c01cca98db503cf65c99d22928ff50
jtk <jtk@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-02-11 17:57:23 +0000

    DO NOT USE: transition to struts2 half done

--- web/src/main/java/isacrodi/struts/HelloAction.java
+++ web/src/main/java/isacrodi/struts/HelloAction.java
@@ -1,36 +1,77 @@
 package isacrodi.struts;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ejb.EJB;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.naming.InitialContext;
+import javax.naming.Context;
 
-import org.apache.struts.action.*; 
-import isacrodi.ejb.session.Hello;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ActionContext;
+import org.apache.struts2.interceptor.SessionAware;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.dispatcher.SessionMap;
+
+import isacrodi.ejb.session.*;
 
 
-public class HelloAction extends Action 
+public class HelloAction extends ActionSupport
 {
-	Hello helloEjb;
+  String name;
+  Hello helloEjb;
 
-	public HelloAction() throws NamingException 
-	{
-		Context ctx = new InitialContext();
-		helloEjb = (Hello) ctx.lookup("HelloBeanLocal");
-	}
-	
-	public ActionForward execute(ActionMapping map, ActionForm form, HttpServletRequest req, HttpServletResponse resp)
-		throws IOException, ServletException 
-		{
-			HelloForm f = (HelloForm) form;
-			String message = helloEjb.sayHello(f.getName());
-			req.setAttribute("message", message);
-			return map.findForward("hello");
-		}
+
+  public HelloAction() throws NamingException 
+  {
+    Context ctx = new InitialContext();
+    this.helloEjb = (Hello) ctx.lookup("isacrodi/HelloBean/local");
+    this.LOG.info("got hello EJB");
+  }
+
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+
+  public String getName()
+  {
+    return (this.name);
+  }
+
+
+  public String execute()
+  {
+    return (SUCCESS);
+  }
+
+
+  public String getEjbHello()
+  {
+    if (this.name == null)
+    {
+      return (this.helloEjb.sayHello("anonymous coward"));
+    }
+    else
+    {
+      return (this.helloEjb.sayHello(this.name));
+    }
+  }
+
+
+  public void validate()
+  {
+    if (this.name.equalsIgnoreCase("hello") || this.name.equalsIgnoreCase("hola"))
+    {
+      this.LOG.info(String.format("got valid name: %s", this.name));
+    }
+    else
+    {
+      this.LOG.info(String.format("got invalid name: %s", this.name));
+      this.addFieldError("name", "invalid 'hello' name (bad language?)");
+    }
+  }
 }
-
--- web/src/main/webapp/index.jsp
+++ web/src/main/webapp/index.jsp
@@ -3,5 +3,6 @@
 <meta http-equiv="refresh" content="0;URL=index.do">
 </head>
 <body>
+<h1>dummy</h1>
 </body>
 </html>
