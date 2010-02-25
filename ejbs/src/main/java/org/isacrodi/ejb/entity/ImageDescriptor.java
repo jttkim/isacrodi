87a688cdb22ff9e3930ccfa00de0fa635799cb12
bkx08wju <bkx08wju@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-02-25 14:55:20 +0000

    Image processing procedure was added and tested onto DiagnosisTest

--- ejbs/src/main/java/org/isacrodi/ejb/entity/ImageDescriptor.java
+++ ejbs/src/main/java/org/isacrodi/ejb/entity/ImageDescriptor.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-
+import java.awt.image.BufferedImage;
 
 /**
   * Implements image descriptor from Digital Image
@@ -16,6 +16,7 @@ public class ImageDescriptor extends Descriptor implements Serializable
   private ImageType imageType;
   private String mimeType;
   private byte[] imageData;
+  private BufferedImage bufferedImage;
 
   private static final long serialVersionUID = 1;
 
@@ -65,11 +66,22 @@ public class ImageDescriptor extends Descriptor implements Serializable
   }
 
 
+  public void setBufferedImage(BufferedImage bufferedImage)
+  {
+    this.bufferedImage = bufferedImage;
+  }
+
+
+  public BufferedImage getBufferedImage()
+  {
+    return (this.bufferedImage);
+  }
+
+
   public void setImageData(byte[] imageData)
   {
     this.imageData = imageData;
   }
-
 
   // method to get image as java.awt... object to encapsulate low-level octet stream and MIME type stuff...
 
