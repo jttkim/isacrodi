d56b711008f4ffd15580d099d5b5bb257c90a78c
jtk <jtk@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-03-22 17:43:20 +0000

    marginal class diagram update DummyDiagnosisProvider passes empty CDR test but fails crop specificity test

--- ejbs/src/main/java/org/isacrodi/diagnosis/DummyDiagnosisProvider.java
+++ ejbs/src/main/java/org/isacrodi/diagnosis/DummyDiagnosisProvider.java
@@ -3,11 +3,16 @@ package org.isacrodi.diagnosis;
 import org.isacrodi.ejb.entity.*;
 import java.io.IOException;
 import java.util.Set;
+import java.util.HashSet;
+
+
 /**
-  * Implements Diagnosis Provider Interface
+ * Implements Diagnosis Provider Interface
  */
 public class DummyDiagnosisProvider implements DiagnosisProvider
 {
+  private Set<CropDisorder> cropDisorderSet;
+
   
   public DummyDiagnosisProvider()
   {
@@ -15,7 +20,34 @@ public class DummyDiagnosisProvider implements DiagnosisProvider
   }
 
 
+  public void setKnownDisorderSet(Set<CropDisorder> cropDisorderSet)
+  {
+    this.cropDisorderSet = cropDisorderSet;
+    for (CropDisorder cdo : this.cropDisorderSet)
+    {
+      System.err.println(cdo);
+    }
+  }
+
+
   public Diagnosis diagnose(CropDisorderRecord cropDisorderRecord)
+  {
+    Diagnosis d = new Diagnosis();
+    d.setCropDisorderRecord(cropDisorderRecord);
+    d.setDisorderScoreSet(new HashSet<DisorderScore>());
+    for (CropDisorder disorder : this.cropDisorderSet)
+    {
+      DisorderScore ds = new DisorderScore();
+      ds.setScore(1.0 / this.cropDisorderSet.size());
+      ds.setDiagnosis(d);
+      ds.setCropDisorder(disorder);
+      d.addDisorderScore(ds);
+    }
+    return (d);
+  }
+
+
+  public Diagnosis diagnoseOld(CropDisorderRecord cropDisorderRecord)
   {
    
     CDRFeatureExtractor c = new DummyCDRFeatureExtractor();
