cccf8ef6408bc6a2f63458366d6d6f31cc4b3bc3
jtk <jtk@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-03-23 16:27:05 +0000

    some more tidying up of hello stuff added WelcomeAction (doesn't do anything useful yet) wrote import/pom.xml to produce standalone jar

---
+++ trunk/web/src/main/java/org/isacrodi/struts/WelcomeAction.java
@@ -0,0 +1,19 @@
+package org.isacrodi.struts;
+
+import javax.naming.NamingException;
+
+
+public class WelcomeAction extends IsacrodiActionSupport
+{
+  public WelcomeAction() throws NamingException
+  {
+    super();
+  }
+
+
+  // FIXME: nothing in here yet
+  public String execute()
+  {
+    return (SUCCESS);
+  }
+}
--- trunk/web/src/main/webapp/WEB-INF/views/hello.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
-
-<html>
-<body>
-<h2>Hello, <bean:write name="name"/>!</h2>
-</body>
-</html>
-
