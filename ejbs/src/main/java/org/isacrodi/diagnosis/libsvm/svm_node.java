d0af5800721281b991b310af79a9a6aba05cf291
bkx08wju <bkx08wju@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-04-26 16:13:06 +0000

    libsvm dependency added, package cannot be found

--- ejbs/src/main/java/org/isacrodi/diagnosis/libsvm/svm_node.java
+++
@@ -1,7 +0,0 @@
-//package libsvm;
-package org.isacrodi.diagnosis.libsvm;
-public class svm_node implements java.io.Serializable
-{
-	public int index;
-	public double value;
-}
--- ejbs/src/main/java/org/isacrodi/diagnosis/libsvm/svm_print_interface.java
+++
@@ -1,6 +0,0 @@
-//package libsvm;
-package org.isacrodi.diagnosis.libsvm;
-public interface svm_print_interface
-{
-	public void print(String s);
-}
