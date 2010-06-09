3fa40a07a84a454813a128c8649bbd6241fc218d
bkx08wju <bkx08wju@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-06-09 13:48:11 +0000

    DummyDiagnosisProvider.java left as it was to avoid code crashing

--- trunk/ejbs/src/main/java/org/isacrodi/diagnosis/FeatureClassifier.java
+++ trunk/ejbs/src/main/java/org/isacrodi/diagnosis/FeatureClassifier.java
@@ -12,6 +12,8 @@ import java.io.*;
 import java.io.ByteArrayOutputStream;
 import libsvm.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
   * Feature classifier
@@ -19,46 +21,33 @@ import java.io.Serializable;
 public class FeatureClassifier implements Serializable
 {
 
-  private svm_parameter param; 
-  private svm_problem prob;
-  private svm_model model;
-  private String input_file_name;
-  private String model_filename;
-  private String error_msg;
-  private int cross_validation;
-  private int nr_fold;
-
-
   public FeatureClassifier()
   {
     super();
   }
 
   
-  public double [][] dummyClassifier(FeatureVector featureVector) 
+  public Collection<ScoreTable> dummyClassifier(FeatureVector featureVector) 
   {
-   double [][] score = null;
-   svm_node[] x = new svm_node[featureVector.size()];
+
+   String model_filename;
+   Collection<ScoreTable> scoreTable = new ArrayList<ScoreTable>();
+   svm_node[] fv = new svm_node[featureVector.size()];
 
    model_filename = "src/test/java/org/isacrodi/diagnosis/isacrodi_model";
 
    try
    {
      svm_model model = svm.svm_load_model(model_filename);
-     int nr_class = svm.svm_get_nr_class(model);
-     int[] labels = new int[nr_class];
-     svm.svm_get_labels(model, labels);
-     createClassifierSettings();
-     x = mapFeature(featureVector);
+     fv = mapFeature(featureVector);
      svm_predict p = new svm_predict();
-     score = p.predict(model, x);
-
+     scoreTable = p.predict(model, fv);
    }
    catch(IOException e) 
    { 
-     System.out.println("Unable to create "+model_filename+": "+e.getMessage());
+     System.out.println("Unable to create " + model_filename + ": " + e.getMessage());
    }
-    return score;
+    return scoreTable;
 
   }
 
@@ -66,113 +55,21 @@ public class FeatureClassifier implements Serializable
   public svm_node[] mapFeature(FeatureVector featureVector)
   {
 
-     MainClass mc = new MainClass();
-     mc.populateSet();
-     mc.readSet();
+    MainClass mc = new MainClass();
+    mc.populateSet();
+    mc.readSet();
 
-     int i = 0;
+    int i = 0;
 
-
-    svm_node[] x = new svm_node[featureVector.size()];
+    svm_node[] fv = new svm_node[featureVector.size()];
     for (String k : featureVector.keySet())
     {
-      x[i] = new svm_node();
-      x[i].index = mc.findFeatureLabel(k);
-      x[i].value = featureVector.get(k);
+      fv[i] = new svm_node();
+      fv[i].index = mc.findFeatureLabel(k);
+      fv[i].value = featureVector.get(k);
       i++;
     }
-    return x;
-  }
-
-
-/*
- * Create classifier settings
-*/
-  public void createClassifierSettings()
-  {
-    param = new svm_parameter();
-    param.svm_type = svm_parameter.C_SVC;
-    param.kernel_type = svm_parameter.RBF;
-    param.degree = 3;
-    param.gamma = 0;
-    param.coef0 = 0;
-    param.nu = 0.5;
-    param.cache_size = 100;
-    param.C = 1;
-    param.eps = 1e-3;
-    param.p = 0.1;
-    param.shrinking = 1;
-    param.probability = 1;
-    param.nr_weight = 0;
-    param.weight_label = new int[0];
-    param.weight = new double[0];
-
-
-  }
-
-
-/**
-Create dummy training data
-
-*/
-  public void loadFeatureVector()
-  {
-
-    Vector<Double> vy = new Vector<Double>();
-    Vector<svm_node[]> vx = new Vector<svm_node[]>();
-    int max_index = 0;
-
-    // For testing purposes only
-
-    double [] a = new double[6];
-    a[0] = 1;
-    a[1] = 1;
-    a[2] = 1;
-    a[3] = 0;
-    a[4] = 0;
-    a[5] = 0;
-
-    for(int i=0; i<6; i++)
-    {
-      vy.addElement(a[i]);
-      int m = 6;
-      svm_node[] x = new svm_node[m];
-      for(int j = 0; j < m; j++)
-      {
-        x[j] = new svm_node();
-        x[j].index = j+1;
-        x[j].value = 10+j;
-      }
-      if(m>0) max_index = Math.max(max_index, x[m-1].index);
-      vx.addElement(x);
-    }
-
-    prob = new svm_problem();
-    prob.l = vy.size();
-    prob.x = new svm_node[prob.l][];
-
-    for(int i = 0; i < prob.l; i++)
-      prob.x[i] = vx.elementAt(i);
-    prob.y = new double[prob.l];
-    for(int i = 0; i < prob.l; i++)
-      prob.y[i] = vy.elementAt(i);
-    if(param.gamma == 0 && max_index > 0)
-      param.gamma = 1.0/max_index;
-
-    if(param.kernel_type == svm_parameter.PRECOMPUTED)
-      for(int i = 0; i < prob.l; i++)
-      {
-        if (prob.x[i][0].index != 0)
-        {
-          System.err.print("Wrong kernel matrix: first column must be 0:sample_serial_number\n");
-          System.exit(1);
-        }
-        if ((int)prob.x[i][0].value <= 0 || (int)prob.x[i][0].value > max_index)
-        {
-          System.err.print("Wrong input format: sample_serial_number out of range\n");
-          System.exit(1);
-        }
-       }
+    return fv;
   }
 
 }
--- trunk/ejbs/src/main/java/org/isacrodi/diagnosis/svm_predict.java
+++ trunk/ejbs/src/main/java/org/isacrodi/diagnosis/svm_predict.java
@@ -3,6 +3,8 @@ package org.isacrodi.diagnosis;
 import java.io.*;
 import java.util.*;
 import libsvm.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 class svm_predict 
@@ -13,40 +15,29 @@ class svm_predict
     super();
   }
   
-  public double[][] predict(svm_model model, svm_node[] x)
+  public Collection<ScoreTable> predict(svm_model model, svm_node[] x)
   {
 
-    double error = 0;
+    Collection<ScoreTable> scoreTable = new ArrayList<ScoreTable>();
 
-    int svm_type = svm.svm_get_svm_type(model);
     int nr_class = svm.svm_get_nr_class(model);
     double[] prob_estimates = null;
-    double [][] score = new double[nr_class][2]; 
     int[] labels = new int[nr_class];
-
+    double v;
 
     svm.svm_get_labels(model,labels);
     prob_estimates = new double[nr_class];
    
-    /*
-    svm_node[] x = new svm_node[featureVector.size()];
-    for(int i = 0; i < featureVector.size(); i++)
-    {
-      x[i] = new svm_node();
-      x[i].index = i+1;
-      x[i].value = featureVector.get(Integer.toString(i+1));
-    }
-    */
-    double v;
-
     v = svm.svm_predict_probability(model, x, prob_estimates);
-     
-    for(int i=0; i < prob_estimates.length; i++) {
-      score[i][0] = labels[i];
-      score[i][1] = prob_estimates[i];
+    for(int i = 0; i < prob_estimates.length; i++) 
+    {
+      ScoreTable st = new ScoreTable();
+      st.setLabel(Integer.toString(labels[i]));
+      st.setScore(prob_estimates[i]);
+      scoreTable.add(st);
     }
    
-  return score;
+  return scoreTable;
   }
 
 
