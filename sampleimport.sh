905f7766f6959d96a1371e40d7ec85a712f7b0fe
jtk <jtk@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-06-23 15:06:24 +0000

    added sampleimport

---
+++ sampleimport.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function do_run ()
+{
+  echo $*
+  if $* ; then
+    true
+  else
+    echo "*** error"
+    exit 1
+  fi
+}
+
+
+do_run cp import/target/isacrodi-import.jar sampledata/
+cd sampledata
+do_run java -jar isacrodi-import.jar isacrodi_users.txt
+do_run java -jar isacrodi-import.jar isacrodi_crops.txt
+do_run java -jar isacrodi-import.jar isacrodi_disorders.txt
+do_run java -jar isacrodi-import.jar isacrodi_numerictypes.txt
+do_run java -jar isacrodi-import.jar isacrodi_imagetypes.txt
+do_run java -jar isacrodi-import.jar isacrodi_cdrs.txt
+
