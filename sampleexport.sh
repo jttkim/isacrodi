bc470e2816fb50dc26061aa0b3dbecea2c4b99e7
bkx08wju <bkx08wju@79551a3d-745a-4530-a2e7-6a9f7d35f76a>
2010-09-13 17:16:58 +0000

    export file

---
+++ sampleexport.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function do_run ()
+{
+  echo $*
+  if $* ; then
+    true
+  else
+    echo "*** error"
+    exit 1
+  fi
+}
+
+do_run cp import/target/isacrodi-import.jar sampledata/
+cd sampledata
+do_run java -jar isacrodi-import.jar -e export_cd.txt
+do_run java -jar isacrodi-import.jar export_cd.txt
